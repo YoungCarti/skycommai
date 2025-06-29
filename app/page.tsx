dac1b9132c3464d21f1348ff2de04b63a5ec6f6f
Resh <saabireshdk@gmail.com>
2025-06-29 23:17:50 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -167,7 +167,7 @@ export default function Home() {
               Hello
             </h1>
             <p className="text-sm text-gray-500 mb-8">
-              No credit card required.
+              No credit card asked!
             </p>
           </div>
         </main>
